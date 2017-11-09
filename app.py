65decbfd611fa8ecd6f83a393d5b4c0179f1a0a2
Gerardo Cruz <gerardocd1000@gmail.com>
2017-11-08 19:16:47 -0600

    fixed

--- app.py
+++ app.py
@@ -29,4 +29,4 @@ def upload_file2():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=8080)
--- opencv.py
+++ opencv.py
@@ -2,7 +2,7 @@ import numpy
 import cv2
 
 # read and downscale image
-img = cv2.pyrDown(cv2.imread('italy.jpg', cv2.IMREAD_UNCHANGED))
+img = cv2.pyrDown(cv2.imread('flor.jpg', cv2.IMREAD_UNCHANGED))
 # threshold image
 # this step is neccessary when you work with contours
 ret, threshed_img = cv2.threshold(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), 245, 255, cv2.THRESH_BINARY)
